49203bdcd04a6b24dd859fc6c47a1c58a894e48a
dWiGhT Mulcahy <dwightmulcahy@gmail.com>
2017-10-27 13:50:54 -0700

    setup stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='keg-gui',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='dWiGhT',
+    author_email='',
+    description='', requires=['pygame', 'pymysql']
+)
